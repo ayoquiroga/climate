c8e001260a012aa1effea857c4eaf79a26792b82
ayoquiroga <ayo.quiroga@gmail.com>
2023-10-21 15:25:08 -0300

    con mapa con iconos de geo

--- src/components/transport/Transport.jsx
+++ src/components/transport/Transport.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
 import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+import { Icon } from "leaflet"
 import './Transport.css'
 import "leaflet/dist/leaflet.css";
+import  data  from "./vehiclePositionsSimple.json"
+import icono from './icono.png'
+import  L from "leaflet";
 
-const position = [-31.53492123277424, -68.52718348583097]
+
 
 const Transport = () => {
+
+  const position = [-34.62226285961549, -58.53152534945992]
+
+  const lineas = data.slice(0,50);
+
+  const customIcon = L.icon({
+    iconUrl: icono,
+    iconSize: [30, 30],
+    iconAnchor: [30, 30],
+  });
+
   return (<div className='leaflet-container' id='map'>
-            <MapContainer center={[-31.53492123277424, -68.52718348583097]} zoom={15} scrollWheelZoom={false}>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
               <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
               />
-              <Marker position={[-31.53492123277424, -68.52718348583097]}>
+              {lineas.map((lineas, index) => 
+                <Marker 
+                  key={index} 
+                  position={[lineas.latitude , lineas.longitude]}
+                  icon={customIcon} >
                 <Popup>
-                  A pretty CSS3 popup. <br /> Easily customizable.
+                key={lineas.agency_id}
                 </Popup>
               </Marker>
+              
+              )}
+ 
             </MapContainer>
           </div>
         )
